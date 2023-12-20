6274c7ccff6067ee93a98c915693525dbcc3142a
James <james.bergacs@vtsd.com>
2023-12-20 09:24:17 -0500

    update

--- 1-2-4/main.py
+++ 1-2-4/main.py
@@ -1,4 +1,5 @@
 import turtle as trtl
+import random as rand
 '''
 x = starting distance
 y = incremental distance
@@ -8,25 +9,44 @@ Loop:
     go forward x + y
     turn left 90 degrees
 '''
-trtl.speed(10)
+ds = 0
+ds = 0
+trtl.speed(0)
 x = 15
 y = 5
 trtl.left(90)
-trtl.width(5)
-
+trtl.width(6)
+iteration = 0
 def Door():
-    trtl.forward(10)
+    global ds
+    trtl.forward(ds)
     trtl.penup()
-    trtl.forward(x * 2)
+    trtl.forward(x * 3)
     trtl.pendown()
+
+def Barrier():
+    global bs
+    trtl.forward(br)
+    trtl.left(90)
+    trtl.forward(x * 2.5)
+    trtl.back(x * 2.5)
+    trtl.right(90)
 
 
 def Spiral():
-    global x, y
+    global x, y, iteration, ds, bs
     for spirals in range(25):
+        ds = rand.randint(x * 2, (y - x * 2))
+        bs = rand.randint(x * 2, (y - x * 2))
+        trtl.penup()
+        if iteration > 3:
+            trtl.pendown()
+        Door()
+        Barrier()
         trtl.forward(x + y)
         trtl.left(90)
         y += 20
+        iteration += 1
     trtl.hideturtle()
 Spiral()
 
